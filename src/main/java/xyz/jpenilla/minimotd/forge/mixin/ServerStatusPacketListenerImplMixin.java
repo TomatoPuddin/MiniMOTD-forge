d82cc6cb7cd8f817796643897931c543ee9249d5
TomatoPuddin <tomato-puddin@outlook.com>
2022-11-04 00:35:34 +0800

    fix mixin error in some cases

--- src/main/java/xyz/jpenilla/minimotd/forge/mixin/ServerStatusPacketListenerImplMixin.java
+++ src/main/java/xyz/jpenilla/minimotd/forge/mixin/ServerStatusPacketListenerImplMixin.java
@@ -29,8 +29,6 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.ServerStatusResponse;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.ITextComponent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -49,7 +47,7 @@ import xyz.jpenilla.minimotd.forge.access.ConnectionAccess;
 @Unique
 @Mixin(net.minecraft.server.network.NetHandlerStatusServer .class)
 abstract class ServerStatusPacketListenerImplMixin {
-  private static final Logger LOGGER = LogManager.getLogger();
+  // private static final Logger LOGGER = LogManager.getLogger();
   @Shadow
   @Final
   private NetworkManager networkManager;
